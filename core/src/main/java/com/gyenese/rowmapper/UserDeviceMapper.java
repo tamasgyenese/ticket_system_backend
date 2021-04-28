09eb907d7c7caf7ced93ac053d12f2a3682952d4
tamasgyenese <gyenesetomi@gmail.com>
2021-04-28 21:16:07 +0200

    adding POJOs and RowMappers

---
+++ core/src/main/java/com/gyenese/rowmapper/UserDeviceMapper.java
@@ -0,0 +1,23 @@
+package com.gyenese.rowmapper;
+
+import com.constans.FieldConstants;
+import com.gyenese.model.UserDevice;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * RowMapper for UserDevice class
+ */
+
+public class UserDeviceMapper implements RowMapper<UserDevice> {
+
+    @Override
+    public UserDevice mapRow(ResultSet rs, int rowNum) throws SQLException {
+        UserDevice userDevice = new UserDevice();
+        userDevice.setUserId(rs.getLong(FieldConstants.DB_FIELD_USER_ID));
+        userDevice.setDeviceHash(rs.getString(FieldConstants.DB_FIELD_DEVICE_HASH));
+        return userDevice;
+    }
+}
---
+++ core/src/main/java/com/gyenese/rowmapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.gyenese.rowmapper;
+
+import com.constans.FieldConstants;
+import com.gyenese.model.User;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * RowMapper for User class
+ */
+
+public class UserMapper implements RowMapper<User> {
+
+    @Override
+    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+        User user = new User();
+        user.setUserId(rs.getLong(FieldConstants.DB_FIELD_USER_ID));
+        user.setName(rs.getString(FieldConstants.DB_FIELD_NAME));
+        user.setEmail(rs.getString(FieldConstants.DB_FIELD_EMAIL));
+        return user;
+    }
+}
